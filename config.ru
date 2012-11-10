6ab391030cd099a31d64febea6695ccbc6e6861a
Joseph Abrahamson <me@jspha.com>
2012-11-09 17:38:54 -0800

    Externalized the language and added some parsers

--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require './hack_sanitation_app'
+R18n::I18n.default = 'en'
 run HackSanitationApp
--- hack_sanitation_app.rb
+++ hack_sanitation_app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/r18n'
 require 'twilio-ruby'
 
 # TODO Validate that Twilio is actually sending these messages
@@ -7,6 +7,9 @@ require 'twilio-ruby'
 # TODO Validate via HTTP Basic
 
 class HackSanitationApp < Sinatra::Base
+
+  register Sinatra::R18n
+  set :root, File.dirname(__FILE__)
 
   # These ENV variables must be configured via 'heroku config'
   @twilio_sid   = ENV['TWILIO_SID']
@@ -19,13 +22,41 @@ class HackSanitationApp < Sinatra::Base
     
     from = params["From"]
     body = params["Body"]
-    
+
     if from and body
-      [200, {}, body]
+      commands = parse_commands t.commands, body
+      tags     = parse_hashtags body
+      [200, {}, tags.join(' ')]
     else
-      # What happened? It wasn't Twilio, apparently
-      404
+      404  # What happened? Not Twilio, apparently
     end
+  end
+
+
+  private
+
+  # Looks for all the command words in the message
+  def parse_commands com, body
+    # Accumulate our command dictionary
+    dict = com.report + com.clean + com.follow + com.stop
+    # Get the words from the body
+    words = body.downcase.split(/\W+/)
+    
+    words & dict
+  end
+
+  # Looks for hashtags like #bang! ==> ['bang!']
+  def parse_hashtags body
+    # Get all the words
+    words = body.downcase.split
+
+    # Delete anything that isn't a hashtag
+    words.delete_if do |w|
+      not (w[0,1] == "#")
+    end
+
+    # Trim off the "#"s
+    words.each { |w| w.slice!(0) }
   end
 
   run! if app_file == $0
